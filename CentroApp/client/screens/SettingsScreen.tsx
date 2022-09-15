ac72987ba4acff795f439174762a85e10ce8513b
Harnish Savsani <68416821+HarnishSavsani@users.noreply.github.com>
2022-09-15 11:41:25 +0530

    Implement more screens and styles

--- CentroApp/client/screens/SettingsScreen.tsx
+++ CentroApp/client/screens/SettingsScreen.tsx
@@ -2,16 +2,18 @@ import * as React from 'react'
 
 import { layout, text } from '../styles/styles'
 
-import { ScrollView } from 'react-native'
-import { Text, Container, Separator } from '../components/Themed'
+import { ScrollView, View } from 'react-native'
+import { Container, Separator, Text } from '../components/ThemedComponents'
 
 export default function SettingsScreen() {
 	return (
-		<ScrollView style={{flexDirection: 'column'}}>
-			<Container style={layout.centered}>
-				<Text style={text.h1}>Settings</Text>
-				<Separator style={layout.hr} />
-			</Container>
+		<ScrollView>
+			<View style={layout.column}>
+				<Container style={layout.centered}>
+					<Text style={text.h1}>Settings</Text>
+					<Separator />
+				</Container>
+			</View>
 		</ScrollView>
 	)
 }
